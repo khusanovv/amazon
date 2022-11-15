459f6969011bc4a6c8272aea23951556cf1c5428
Boburshoh Khusanov <110517357+khusanovv@users.noreply.github.com>
2022-11-15 15:31:45 +0500

    add new sell page

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,26 +1,30 @@
-import { Route } from 'react-router-dom';
+import { Route } from "react-router-dom";
 import Home from "./home/Home";
 import Login from "./login/Login";
 import SignUp from "./signup/SignUp";
-import CustomerService from './customer-service/CustomerServices'
+import CustomerService from "./customer-service/CustomerServices";
+import Sell from "./sell/Sell";
 
 const Routes = () => {
   return (
     <>
       <Route exact path="/">
-        <Home/>
+        <Home />
       </Route>
       <Route path="/login">
-          <Login/>
+        <Login />
       </Route>
       <Route path="/signup">
-          <SignUp/>
+        <SignUp />
       </Route>
       <Route path="/customer-service">
         <CustomerService />
       </Route>
+      <Route path="/sell">
+        <Sell />
+      </Route>
     </>
-  )
-}
+  );
+};
 
-export default Routes
+export default Routes;
